655e2007deeb1f55f21207f96d39f24e879605a9
vitor795 <eurich.vitor@escola.pr.gov.br>
2026-09-17 11:44:58 -0300

    diurda 01

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ const perguntas = [
     },
     {
        
-            enunciado: "As fontes renováveis de energia são aquelas formas de produção de energia em que suas fontes são capazes de manter-se disponíveis durante um longo prazo, contando com recursos que se regeneram ou que se mantêm ativos permanentemente. Com base nisso, temos a energia geotérmica, que consiste em utilizar o calor manifestado em áreas próximas à superfície. Qual a sua opinião sobre esse modelo de energia renovável?",
+            enunciado: "Enfrentamento de Surtos e Falta de Saneamento Básico",
             alternativas: [
                 {
-                    texto: "A energia geotérmica precisa ser mais explorada visto que utiliza o calor interno da terra, pois não depende do clima e é inesgotável.",
-                    afirmacao: "Você demonstra um perfil  de pesquisador e inovador que busca por meio da inovação soluções para o meio ambiente e sustentabilidade das gerações posteriores."
+                    texto: "Em um município de baixa renda com alto índice de contaminação por doenças associadas à ausência de saneamento básico (como dengue, leptospirose e verminoses), qual deve ser a estratégia de intervenção priorizada pela gestão municipal?",
+                    afirmacao: " Fortalecer a atuação imediata dos Agentes Comunitários de Saúde (ACS) e dos Agentes de Combate às Endemias (ACE), promovendo mutirões de conscientização, eliminação de focos de contaminação e distribuição direta de insumos de proteção à população."
                 },
                 {
-                    texto:  "Seria interessante explorar primeiramente fontes renováveis exotérmicas, em segundo plano utilizar a energia geotérmica apesar dos custos elevados de explorações e possibilidades de contaminação de rios. ",
+                    texto:  "Articular investimentos e parcerias para obras públicas de grande porte voltadas ao saneamento básico, canalização de esgoto e drenagem urbana nas áreas periféricas.",
                     afirmacao: "Você é uma pessoa que dá ênfase em priorizar os recursos disponíveis, pensando no meio ambiente e sem descartar o viés econômico."
                 }    
                
