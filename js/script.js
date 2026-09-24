bda438489d3b3c986c0fd1fb827fc44f47ca8188
vitor795 <eurich.vitor@escola.pr.gov.br>
2026-09-24 12:03:17 -0300

    aura

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 let atual = 0;
 let perguntaAtual;
